b5527b35108c6001313ac397c238a7abd4ff7ac0
조유신 <cho8wola@sju.ac.kr>
2018-07-10 03:35:47 +0900

    ServicesListAdapter에 이미지 뷰 추가, 구

--- app/src/main/java/com/sejongsoftware/seoulappproject/ServicesListAdapter.java
+++ app/src/main/java/com/sejongsoftware/seoulappproject/ServicesListAdapter.java
@@ -1,16 +1,22 @@
 package com.sejongsoftware.seoulappproject;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -22,6 +28,7 @@ public class ServicesListAdapter extends BaseAdapter {
     Context context;
     int layout;
     ArrayList<Item> alist;
+    Bitmap bitmap;
 
     public ServicesListAdapter(Context context, int layout, ArrayList<Item> alist)
     {
@@ -52,10 +59,38 @@ public class ServicesListAdapter extends BaseAdapter {
 
         TextView tv_item_name = (TextView) convertView.findViewById(R.id.item_label);
         TextView tv_item_state = (TextView) convertView.findViewById(R.id.item_state);
+        ImageView tv_item_img = (ImageView) convertView.findViewById(R.id.item_img);
         LinearLayout item_layout = (LinearLayout) convertView.findViewById(R.id.item_layout);
 
         tv_item_name.setText(alist.get(position).getSVCNM());
         tv_item_state.setText(alist.get(position).getSVCSTATNM());
+
+        Thread mThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(alist.get(position).getIMGURL());
+
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream();
+                    bitmap = BitmapFactory.decodeStream(is);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mThread.start();
+
+        try {
+            mThread.join();
+            tv_item_img.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return convertView;
     }
